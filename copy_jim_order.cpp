31df170de2a0e8d69e392f0de5605efead959dec
amankayat <72142181+amankayat@users.noreply.github.com>
2021-11-16 13:38:20 +0530

    best approach using brute force

--- copy_jim_order.cpp
+++ copy_jim_order.cpp
@@ -1,116 +1,58 @@
-#include <bits/stdc++.h>
-
+#include<iostream>
 using namespace std;
-
-string ltrim(const string &);
-string rtrim(const string &);
-vector<string> split(const string &);
-
-/*
- * Complete the 'jimOrders' function below.
- *
- * The function is expected to return an INTEGER_ARRAY.
- * The function accepts 2D_INTEGER_ARRAY orders as parameter.
- */
-
-vector<int> jimOrders(vector<vector<int>> orders) {
-    vector<pair<int,int>> result;
-    vector<int> index;
-    int sum=0;
-    for(int i=0;i<orders.size();i++){
-        for(int j=0;j<orders[i].size();j++){
-            sum+=orders[i][j];
+int main(){
+    int n;
+    cin>>n;
+    int a[n][2];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<2;j++){
+            cin>>a[i][j];
         }
-        result.push_back(make_pair(sum,i+1));
+    }
+    int sum=0;
+    int temp[n];
+        for(int i=0;i<n;i++){
+        for(int j=0;j<2;j++){
+            sum+=a[i][j];
+        }
+        temp[i] = sum;
         sum=0;
     }
+    int copy_temp[n];
+    for(int i=0;i<n;i++)
+    copy_temp[i] = temp[i];
     
-    sort(result.begin(),result.end());
-    for(int i=0;i<orders.size();i++){
-        index.push_back(result[i].second);
-    }
-    return index;
-}
-
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    string n_temp;
-    getline(cin, n_temp);
-
-    int n = stoi(ltrim(rtrim(n_temp)));
-
-    vector<vector<int>> orders(n);
-
-    for (int i = 0; i < n; i++) {
-        orders[i].resize(2);
-
-        string orders_row_temp_temp;
-        getline(cin, orders_row_temp_temp);
-
-        vector<string> orders_row_temp = split(rtrim(orders_row_temp_temp));
-
-        for (int j = 0; j < 2; j++) {
-            int orders_row_item = stoi(orders_row_temp[j]);
-
-            orders[i][j] = orders_row_item;
+    
+    for(int i=1;i<n;i++){
+        for(int j=i;j>0;j--){
+            if(temp[j] < temp[j-1]){
+                int var = temp[j];
+                temp[j] = temp[j-1];
+                temp[j-1] = var;
+            }
         }
     }
 
-    vector<int> result = jimOrders(orders);
 
-    for (size_t i = 0; i < result.size(); i++) {
-        fout << result[i];
-
-        if (i != result.size() - 1) {
-            fout << " ";
-        }
+    
+    int index[n];
+    int l=0;
+for(int i=0;i<n;i++)
+{    for(int j=0;j<n;j++){
+    
+    if(temp[i]==copy_temp[j]){
+            index[l] = j+1;
+        l++;
+        copy_temp[j] = -1;
+        break;
     }
 
-    fout << "\n";
-
-    fout.close();
-
-    return 0;
 }
+    
+    
 
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
 }
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
+//display
+for(int i=0;i<l;i++)
+cout<<index[i]<<" ";
 }
-
-vector<string> split(const string &str) {
-    vector<string> tokens;
-
-    string::size_type start = 0;
-    string::size_type end = 0;
-
-    while ((end = str.find(" ", start)) != string::npos) {
-        tokens.push_back(str.substr(start, end - start));
-
-        start = end + 1;
-    }
-
-    tokens.push_back(str.substr(start));
-
-    return tokens;
-}
-
